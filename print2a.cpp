7b71e49dc9c2b7f758a05f24c538f1135ef789ff
earl-stephens <stephensearl@hotmail.com>
2020-06-04 21:05:50 -0400

    C++ Without Fear ch1 exercises

---
+++ print2a.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Test using a different\n";
+  cout << "Way of going to a new line";
+
+  return 0;
+}
---
+++ print2b.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "I am Blaxxon, ";
+  cout << "the godlike computer. ";
+  cout << "Fear me!" << endl;
+
+  return 0;
+}
